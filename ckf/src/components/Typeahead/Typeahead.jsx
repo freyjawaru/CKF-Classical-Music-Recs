0547caad53705af2813cfaf94fa569e849193ca5
Katharine Chandler <katharinechandler@katharines-mbp.home>
2021-01-25 15:31:26 -0500

    fixed css

--- ckf/src/components/Typeahead/Typeahead.jsx
+++ ckf/src/components/Typeahead/Typeahead.jsx
@@ -2,17 +2,14 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Typeahead.css";
 
-
-
 function Typeahead(props) {
   // we need a state to store the current value of our input to use as a filter option
   const [name, setName] = useState("");
   const [category, setCategory] = useState("composer");
   // const [recordings, setRecordings] = useState("");
-  
+
   console.log(category);
-  
-  
+
   useEffect(() => {
     const lastCategory = localStorage.getItem("category") || "composer";
     setCategory(lastCategory);
@@ -23,14 +20,34 @@ function Typeahead(props) {
     localStorage.setItem("category", category);
   }, [name, category]);
   // all items in source array (from props) where the name partially matches the name state
-  const filteredItems = props.source.filter((item) =>
-    item.fields[category] && item.fields[category].toString().toLowerCase().includes(name.toLowerCase())
+  const filteredItems = props.source.filter(
+    (item) =>
+      item.fields[category] &&
+      item.fields[category]
+        .toString()
+        .toLowerCase()
+        .includes(name.toLowerCase())
   );
-  console.log(filteredItems)
-  const categories = ["composer", "work1", "work2", "work3", "conductor", "performingGroupA", "performingGroupB", "soloistA", "soloistAInstrument", "soloistB", "soloistBInstrument", "soloistC", "soloistCInstrument", "label", "year", "notes"];
+  console.log(filteredItems);
+  const categories = [
+    "composer",
+    "work1",
+    "work2",
+    "work3",
+    "conductor",
+    "performingGroupA",
+    "performingGroupB",
+    "soloistA",
+    "soloistAInstrument",
+    "soloistB",
+    "soloistBInstrument",
+    "soloistC",
+    "soloistCInstrument",
+    "label",
+    "year",
+    "notes",
+  ];
 
-  
-  
   return (
     <form className="typeahead">
       <input value={name} onChange={(e) => setName(e.target.value)} />
@@ -39,30 +56,18 @@ function Typeahead(props) {
           <option value={cat}>{cat}</option>
         ))}
       </select>
-      
+
       {name && (
         <div className="show-container">
-          {filteredItems.map((
-            item,
-            index
-          ) => (
-
-            
+          {filteredItems.map((item, index) => (
             <Link key={index} to={`/recordings/${item.id}`}>
               {item.fields.work1} - {item.fields[category]}
-      
             </Link>
-             
-          ))
-         
-        }
-          </div>
-          
+          ))}
+        </div>
       )}
     </form>
-    
   );
-
 }
-  
+
 export default Typeahead;
